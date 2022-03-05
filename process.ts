32bbb1d5936a72d00748be469e5dc5949c27bbbe
Lemasc Service <lemascserviceth@hotmail.com>
2022-03-06 00:22:45 +0700

    Add names and count

--- process.ts
+++ process.ts
@@ -11,14 +11,58 @@ interface BaseExamModel {
   subject: string;
 }
 
-type ExamModel = Partial<BaseExamModel> & {
+type ExamModel = BaseExamModel & {
   id: string;
+  canShowName?: boolean;
 };
 
-function sortByName<T extends ExamModel>(input: T[]): T[] {
+function sortByName<T extends Partial<ExamModel>>(input: T[]): T[] {
   return input.sort((a, b) =>
     (a.name as string) > (b.name as string) ? -1 : 1
   );
+}
+
+type RecursiveData = {
+  count: number;
+  names: string[];
+};
+
+async function readRecursive(dir: string, level: number) {
+  const indexFile = JSON.parse(
+    await fs.readFile(path.join(dir, "index.json"), {
+      encoding: "utf-8",
+    })
+  ) as ExamModel;
+  const data: RecursiveData = { count: 0, names: [] };
+  if (level === 1 && indexFile.items?.length !== 0) {
+    data.names.push(indexFile.name);
+  }
+  if (level !== 0) {
+    console.log(
+      `${Array(level + 1)
+        .fill("====")
+        .join("")}> (${level}) Show ${
+        indexFile.canShowName ? "TRUE" : "FALSE"
+      } | Random ${indexFile.allowRandom ? "TRUE" : "FALSE"} Section ${
+        indexFile.name
+      }`
+    );
+  }
+  if (indexFile.items) {
+    data.count = data.count + Object.keys(indexFile.items).length;
+  }
+  const folders = JSON.parse(
+    await fs.readFile(path.join(dir, "sections.json"), {
+      encoding: "utf-8",
+    })
+  ) as string[];
+
+  for (let sub of folders) {
+    const result = await readRecursive(path.join(dir, sub), level + 1);
+    data.count = data.count + result.count;
+    data.names = [...data.names, ...result.names];
+  }
+  return data;
 }
 
 async function main() {
@@ -31,48 +75,39 @@ async function main() {
     (c) => c.isDirectory() && c.name !== "node_modules" && c.name !== ".git"
   );
   console.log(`Founded ${dir.length} folders`);
-  const rootIndex = (
-    await Promise.all(
-      dir.map<Promise<ExamModel | undefined>>(async (folder) => {
-        try {
-          console.log(`--> Processing folder: ${folder.name} `);
-          const index = JSON.parse(
-            await fs.readFile(path.join(root, folder.name, "index.json"), {
-              encoding: "utf-8",
-            })
-          ) as ExamModel;
-          if (index.name) {
-            console.log;
-            delete index.name;
-            await fs.writeFile(
-              path.join(root, folder.name, "index.json"),
-              JSON.stringify(index)
-            );
-          }
-          return {
-            subject: index.subject,
-            level: index.level,
-            time: index.time,
-            id: folder.name,
-          };
-        } catch (err) {
-          console.error(err);
-          console.log(`--> Error while processing ${folder.name}. Skipping.`);
-        }
-      })
-    )
-  ).filter((v) => v !== undefined) as ExamModel[];
-  console.log(`Processed ${rootIndex.length} folders.`);
-  console.log("Reordering....");
+  const rootIndex: Partial<ExamModel>[] = [];
 
-  const SECONDARY = rootIndex.filter((c) => c.level === "SECONDARY");
-  const UPPER_SECONDARY = rootIndex.filter(
-    (c) => c.level === "UPPER_SECONDARY"
-  );
-  await fs.writeFile(
-    "index.json",
-    JSON.stringify([...sortByName(SECONDARY), ...sortByName(UPPER_SECONDARY)])
-  );
+  for (let folder of dir) {
+    try {
+      console.log(`--> Processing folder: ${folder.name} `);
+      const index = JSON.parse(
+        await fs.readFile(path.join(root, folder.name, "index.json"), {
+          encoding: "utf-8",
+        })
+      ) as ExamModel;
+      if (index.name) {
+        delete (index as Partial<ExamModel>).name;
+        await fs.writeFile(
+          path.join(root, folder.name, "index.json"),
+          JSON.stringify(index)
+        );
+      }
+      rootIndex.push({
+        subject: index.subject,
+        level: index.level,
+        time: index.time,
+        id: folder.name,
+        ...(await readRecursive(path.join(root, folder.name), 0)),
+      });
+    } catch (err) {
+      console.error(err);
+      console.log(`--> Error while processing ${folder.name}. Skipping.`);
+    }
+    console.log("\n");
+  }
+  console.log(`Processed ${rootIndex.length} folders.`);
+
+  await fs.writeFile("index.json", JSON.stringify(sortByName(rootIndex)));
 }
 
 main().catch(console.error);
